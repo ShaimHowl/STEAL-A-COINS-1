bf965736d205d53d3ecf68341abd812534e9455b
«ShaimHowl» <showlader@elpuig.xeill.net>
2025-11-24 10:40:56 +0100

    treachery

--- Scripts/jugador.gd
+++ Scripts/jugador.gd
@@ -13,38 +13,35 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# ---- ATAQUE 1 ----
+	
 	if Input.is_action_just_pressed("Attack") and not attacking:
 		attacking = true
 		animated_sprite_2d.play("Attack")
 		 
-	# ---- ATAQUE 2 ----
+	
 	if Input.is_action_just_pressed("Attack2") and not attacking:
 		attacking = true
 		animated_sprite_2d.play("Attack2")
 
-	# Si está atacando, esperar a que termine la animación
+	
 	if attacking:
-		# Cuando termine la animación (asegúrate de que NO esté en loop)
 		if not animated_sprite_2d.is_playing():
 			attacking = false
-		# PERO dejar que pueda seguir moviéndose
-		# Así que NO bloqueamos movimiento, solo animación
 		pass
 
-	# ---- SALTO ----
+	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump
   
 	var direction = Input.get_axis("move_left", "move_right")
 
-	# Flip horizontal
+	
 	if direction < 0:
 		animated_sprite_2d.flip_h = true
 	elif direction > 0:
 		animated_sprite_2d.flip_h = false
 
-	# Animaciones de movimiento (solo si NO está atacando)
+	
 	if not attacking:
 		if is_on_floor():
 			if direction == 0:
@@ -57,7 +54,7 @@ func _physics_process(delta):
 			else:
 				animated_sprite_2d.play("fall")
 
-	# Movimiento horizontal (siempre permitido)
+	
 	if direction:
 		velocity.x = direction * speed
 	else:
