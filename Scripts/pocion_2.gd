35b7f3ef1126ecc362829fede2541a5ea3e1c803
ShaimHowl <showlader@elpuig.xeill.net>
2026-02-01 19:35:19 +0100

    nivel5

---
+++ Scripts/pocion_2.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@export var cantidad_cura: int = 10
+@onready var sonido = $AudioStreamPlayer2D
+
+func _ready():
+	connect("body_entered", Callable(self, "_on_body_entered"))
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("jugador"):
+		body.curar(cantidad_cura)
+		sonido.play()
+		hide() # desaparece visualmente
+		set_deferred("monitoring", false) # evita volver a activarse
+		await sonido.finished
+		queue_free()
