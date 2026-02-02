3274c7d8f4c61d97bb2ac7da4826649ac411f32e
ShaimHowl <showlader@elpuig.xeill.net>
2026-02-02 18:29:15 +0100

    nivel5

--- Scripts/misionfallida.gd
+++ Scripts/misionfallida.gd
@@ -18,3 +18,10 @@ func _on_button_pressed() -> void:
 	GameData.monedas = 0
 	GameData.monedas_recogidas.clear()
 	get_tree().change_scene_to_file("res://Scenes/mundo.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	GameData.health = GameData.max_health
+	GameData.monedas = 0
+	GameData.monedas_recogidas.clear()
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
