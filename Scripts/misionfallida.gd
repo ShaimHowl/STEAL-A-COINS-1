54403ae9f49604eefd1d921a1799382d98f5919f
ShaimHowl <showlader@elpuig.xeill.net>
2026-02-10 18:38:01 +0100

    Decoracion

--- Scripts/misionfallida.gd
+++ Scripts/misionfallida.gd
@@ -28,9 +28,14 @@ func animar_texto():
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/mundo.tscn")
-
+	GameData.health = GameData.max_health
+	GameData.monedas = 0
+	GameData.monedas_recogidas.clear()
 
 
 
 func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+	GameData.health = GameData.max_health
+	GameData.monedas = 0
+	GameData.monedas_recogidas.clear()
--- Scripts/pocion_2.gd
+++ Scripts/pocion_2.gd
@@ -3,8 +3,7 @@ extends Area2D
 @export var cantidad_cura: int = 15
 @onready var sonido = $AudioStreamPlayer2D
 
-func _ready():
-	connect("body_entered", Callable(self, "_on_body_entered"))
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("jugador"):
