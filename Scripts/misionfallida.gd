3fb880b6f8cc20cda9159852e6a3e2147425fc59
«ShaimHowl» <showlader@elpuig.xeill.net>
2026-02-10 12:15:37 +0100

    hola

--- Scripts/misionfallida.gd
+++ Scripts/misionfallida.gd
@@ -28,3 +28,9 @@ func animar_texto():
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/mundo.tscn")
+
+
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
