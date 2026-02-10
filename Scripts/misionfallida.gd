245c03fc1da4485ca78333e049ee816fd0c9a3b0
«ShaimHowl» <showlader@elpuig.xeill.net>
2026-02-10 10:54:42 +0100

    hola

--- Scripts/misionfallida.gd
+++ Scripts/misionfallida.gd
@@ -4,24 +4,22 @@ extends CanvasLayer
 @onready var texto = $LabelTexto
 @onready var sonidoF = $SonidoMisionF
 
+var texto_completo := ""
+var velocidad := 0.05 # cuanto menor, más rápido
+
 func _ready():
 	titulo.text = "MISIÓN FALLIDA"
 	sonidoF.play()
 
-	texto.text = "Has cogido %d de %d monedas" % [
+	texto_completo = "Has cogido %d de %d monedas" % [
 		GameData.monedas,
 		GameData.monedas_totales
 	]
 
-func _on_button_pressed() -> void:
-	GameData.health = GameData.max_health
-	GameData.monedas = 0
-	GameData.monedas_recogidas.clear()
-	get_tree().change_scene_to_file("res://Scenes/mundo.tscn")
+	texto.text = ""
+	animar_texto()
 
-
-func _on_button_2_pressed() -> void:
-	GameData.health = GameData.max_health
-	GameData.monedas = 0
-	GameData.monedas_recogidas.clear()
-	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+func animar_texto():
+	for i in texto_completo.length():
+		texto.text += texto_completo[i]
+		await get_tree().create_timer(velocidad).timeout
